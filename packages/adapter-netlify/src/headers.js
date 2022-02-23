423cac9df1fdb3ae2ae1b79ff05fdb4e3fcb58a5
Timothy Foster <tf.auroratide@gmail.com>
2022-02-23 14:33:39 -0600

    Extract split_headers to its own file so it can be unit tested

---
+++ packages/adapter-netlify/src/headers.js
@@ -0,0 +1,28 @@
+/**
+ * Splits headers into two categories: single value and multi value
+ * @param {Headers} headers
+ * @returns {{
+ *   headers: Record<string, string>,
+ *   multiValueHeaders: Record<string, string[]>
+ * }}
+ */
+export function split_headers(headers) {
+	/** @type {Record<string, string>} */
+	const h = {};
+
+	/** @type {Record<string, string[]>} */
+	const m = {};
+
+	headers.forEach((value, key) => {
+		if (key === 'set-cookie') {
+			m[key] = value.split(', ');
+		} else {
+			h[key] = value;
+		}
+	});
+
+	return {
+		headers: h,
+		multiValueHeaders: m
+	};
+}
