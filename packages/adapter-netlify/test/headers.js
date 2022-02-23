cd93949a43371540fbada7d8fb3cddbec0a65d6e
Timothy Foster <tf.auroratide@gmail.com>
2022-02-23 14:47:22 -0600

    Include tests for the existing split_headers function to ensure existing behaviour remains intact

--- packages/adapter-netlify/test/headers.js
+++ packages/adapter-netlify/test/headers.js
@@ -16,4 +16,36 @@ test('empty headers', () => {
     });
 })
 
+test('single-value headers', () => {
+    const headers = new Headers();
+    headers.append('Location', '/apple');
+    headers.append('Content-Type', 'application/json');
+
+    const result = split_headers(headers);
+
+    assert.equal(result, {
+        headers: {
+            // Note: becomes lowercase even if specified as uppercase
+            'location': '/apple',
+            'content-type': 'application/json',
+        },
+        multiValueHeaders: {},
+    });
+})
+
+test('multi-value headers', () => {
+    const headers = new Headers();
+    headers.append('Set-Cookie', 'flavor=sugar');
+    headers.append('Set-Cookie', 'diameter=6cm');
+
+    const result = split_headers(headers);
+
+    assert.equal(result, {
+        headers: {},
+        multiValueHeaders: {
+            'set-cookie': ['flavor=sugar', 'diameter=6cm'],
+        },
+    });
+})
+
 test.run()
